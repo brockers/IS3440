c74d594b7fab5d76c99376ef41d5ee46eb4fa7e5
Bob Rockers <bob@rockerssoft.org>
2016-04-08 17:11:41 -0500

    More fixes

--- docs/expl3/app.js
+++ docs/expl3/app.js
@@ -1,33 +1,25 @@
 $( document ).ready( function() {
+	var authList = {};
 	$.getJSON( "data.json", function( data ) {
 
-		var authList = {};
 		$.each( data, function(key,val){
 			authlist[key]= val;
 		});
 		
 	}
-		function validateForm(){
-			var x=document.forms["login"]["username"].value;
-			var y=document.forms["login"]["password"].value;
-			if ( x==null || x=="" || y==null || y=="" ){
-				alert("Username or password was left blank...");
-				return false;
-			} else {
-				if( authList[x][0] === y ) {
-					window.location.href = authList[x][1];
-				} else {
-					alert("Bad username or password... please try again.");
-				}
-			}
-		}
 
-	if ( "login_success" in args ) {
-		if ( args["login_success"] === 'NO' ) {
-			console.log("Bad username or password.  try again");
-			$(".messages").show();
+	function validateForm(){
+		var x=document.forms["login"]["username"].value;
+		var y=document.forms["login"]["password"].value;
+		if ( x==null || x=="" || y==null || y=="" ){
+			alert("Username or password was left blank...");
+			return false;
 		} else {
-			window.location.href = "./22_" + args['login_success'] + "2.html";
+			if( authList[x][0] === y ) {
+				window.location.href = authList[x][1];
+			} else {
+				alert("Bad username or password... please try again.");
+			}
 		}
 	}
 });
